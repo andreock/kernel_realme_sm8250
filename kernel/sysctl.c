1e3c378a3c319853cba3b56d81a1ed9f9f8a6a10
Daniel Micay <danielmicay@gmail.com>
2017-05-16 17:51:48 -0400

    Android: add toggle for disabling newly added USB devices
    Based on the public grsecurity patches.
    
    Change-Id: I2cbea91b351cda7d098f4e1aa73dff1acbd23cce
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -122,6 +122,9 @@ extern int sysctl_ob_control_handler(struct ctl_table *table, int write, void __
 #endif
 
 /* External variables not in a header file. */
+#ifdef CONFIG_USB
+extern int deny_new_usb;
+#endif
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
 extern int core_uses_pid;
@@ -1332,6 +1335,17 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec_minmax_sysadmin,
 		.extra1		= &zero,
 		.extra2		= &two,
+	},
+#endif
+#ifdef CONFIG_USB
+	{
+		.procname	= "deny_new_usb",
+		.data		= &deny_new_usb,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
+		.extra1		= &zero,
+		.extra2		= &one,
 	},
 #endif
 	{
